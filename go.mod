c19df902e2e11c2570d0ddf3b739491185600e6f
Vilsol <me@vil.so>
2022-05-18 06:38:03 +0300

    use d drive instead

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/otiai10/gosseract/v2 v2.3.1
+	gocv.io/x/gocv v0.30.0
 )
 
 require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	_ "embed"
 	"github.com/disintegration/imaging"
 	"github.com/otiai10/gosseract/v2"
+	"gocv.io/x/gocv"
+	"image"
 	"image/png"
 )
 
@@ -23,11 +25,8 @@ func main() {
 		panic(err)
 	}
 
-	src := imaging.Grayscale(img)
-	src = imaging.Sharpen(src, 4)
-
 	buff := new(bytes.Buffer)
-	if err := png.Encode(buff, src); err != nil {
+	if err := png.Encode(buff, img); err != nil {
 		panic(err)
 	}
 
@@ -42,4 +41,42 @@ func main() {
 
 	println("Result:")
 	println(text)
+
+	cropped := imaging.Crop(img, image.Rect(50, 50, 250, 250))
+	find, err := Find(img, cropped)
+	if err != nil {
+		panic(err)
+	}
+	println("Found:")
+	println(find.String())
+}
+
+func Find(static image.Image, dynamic image.Image) (image.Point, error) {
+	matStatic, err := gocv.ImageToMatRGB(static)
+	if err != nil {
+		return image.Point{}, err
+	}
+
+	grayStatic := gocv.NewMat()
+	gocv.CvtColor(matStatic, &grayStatic, gocv.ColorRGBToGray)
+
+	matDynamic, err := gocv.ImageToMatRGB(dynamic)
+	if err != nil {
+		return image.Point{}, err
+	}
+
+	grayDynamic := gocv.NewMat()
+	gocv.CvtColor(matDynamic, &grayDynamic, gocv.ColorRGBToGray)
+
+	m := gocv.NewMat()
+	result := gocv.NewMat()
+	gocv.MatchTemplate(grayStatic, grayDynamic, &result, gocv.TmCcoeffNormed, m)
+
+	_, _, _, location := gocv.MinMaxLoc(result)
+	_ = result.Close()
+	_ = m.Close()
+	_ = matDynamic.Close()
+	_ = grayDynamic.Close()
+
+	return location, nil
 }
