fdfb2cd0406f56c2771ca7cb4814c9ee931705a5
Vilsol <me@vil.so>
2022-05-20 03:23:59 +0300

    check every path manually

--- dll-copy/main.go
+++ dll-copy/main.go
@@ -12,7 +12,6 @@ import (
 )
 
 var lineRegex = regexp.MustCompile(`(?m)^\s+(.+?)\s=>\s(.+?)\s\(`)
-var diskRegex = regexp.MustCompile(`^(\w):`)
 
 func main() {
 	flag.Parse()
@@ -26,19 +25,6 @@ func main() {
 	pathEnv := os.Getenv("PATH")
 	pathSplit := strings.Split(pathEnv, ";")
 
-	cleanPath := make([]string, len(pathSplit))
-	for i, p := range pathSplit {
-		clean := strings.Replace(p, "\\", "/", -1)
-		diskMatch := diskRegex.FindAllStringSubmatch(p, -1)
-		if len(diskMatch) > 0 {
-			clean = "/" + diskMatch[0][1] + clean[2:]
-		}
-		clean = strings.ToLower(clean)
-
-		println("PATH:", clean)
-		cleanPath[i] = clean
-	}
-
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -47,15 +33,17 @@ func main() {
 	matches := lineRegex.FindAllSubmatch(stdout, -1)
 	for _, match := range matches {
 		println("Found match:", string(match[0]))
-		for _, p := range cleanPath {
-			if strings.HasPrefix(string(match[2]), p) {
-				src := string(match[2])
-				src = src[1:2] + ":" + src[2:]
-				dst := path.Join(wd, "out", string(match[1]))
-				println("Copying", src, "=>", dst)
-				if _, err := copyFile(src, dst); err != nil {
-					panic(err)
-				}
+		for _, p := range pathSplit {
+			src := path.Join(p, string(match[1]))
+
+			if _, err := os.Stat(src); err != nil {
+				continue
+			}
+
+			dst := path.Join(wd, "out", string(match[1]))
+			println("Copying", src, "=>", dst)
+			if _, err := copyFile(src, dst); err != nil {
+				panic(err)
 			}
 		}
 	}
